199fb752b393adbe1d903adfbed7a92385d6f5ae
sezan <sezan92>
2022-05-10 21:59:10 +0900

    utility function script

---
+++ reinforce/util.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+
+def plot_scores(scores):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(1, len(scores)+1), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
