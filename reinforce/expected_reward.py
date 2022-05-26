be10f28ba3cabab64d06e210069ac00e9d307410
sezan <sezan92>
2022-05-26 18:07:11 +0900

    unknown change, committed for record

--- reinforce/expected_reward.py
+++ reinforce/expected_reward.py
@@ -1,12 +1,12 @@
 # TODO: get expected reward, test if it is working
-def get_expected_reward(rewards):
+def get_expected_reward(rewards, gamma_init=1):
     expected_rewards = []
     for i in range(len(rewards)):
         expected_reward = 0
         gamma = 1
         for reward in rewards[i:]: 
             expected_reward += gamma * reward
-            gamma = gamma * 0.99
+            gamma = gamma * gamma_init
         expected_rewards.append(expected_reward)
     return expected_rewards
 
